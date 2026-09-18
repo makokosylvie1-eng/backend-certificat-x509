44f882cfd0bfb1a8e5db6001be8a621e588eb89a
MAKOKO MOPELEMA Sylvie <makokosylvie1@gmail.com>
2026-09-18 13:34:09 +0100

    fix: placer index.php a la racine du dossier public

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Toutes les routes qui ne commencent pas par /api affichent l'interface Angular
+Route::get('/{any?}', function () {
+    $path = public_path('index.html');
+    if (file_exists($path)) {
+        return file_get_contents($path);
+    }
+    return response()->json(['message' => 'Interface Angular non compilée dans public/'], 404);
+})->where('any', '.*');
